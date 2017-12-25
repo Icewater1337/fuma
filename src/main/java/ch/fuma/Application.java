d82c908d4fc9581855ff7d1a9a84ca9e9ac8a45f
Mathias Fuchs <fuchsmath@gmail.com>
2017-12-25 02:36:29 +0100

    added some security measures

--- src/main/java/ch/fuma/Application.java
+++ src/main/java/ch/fuma/Application.java
@@ -1,5 +1,6 @@
 package ch.fuma;
 
+import ch.fuma.controller.MainController;
 import ch.fuma.utility.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,9 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		MainController.lockedDate.clear();
+		MainController.ipAddressesAndCalls.clear();
 
 		ApiContextInitializer.init();
 
--- src/main/java/ch/fuma/controller/MainController.java
+++ src/main/java/ch/fuma/controller/MainController.java
@@ -10,10 +10,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 
 @Controller
 public class MainController {
+
+    public static HashMap<String, Integer> ipAddressesAndCalls = new HashMap<>();
+    public static HashMap<String, String> lockedDate = new HashMap<>();
 
     @GetMapping("/")
     public String greetingForm(Model model, HttpServletRequest request) {
@@ -23,8 +29,31 @@ public class MainController {
 
     @PostMapping("/")
     public String greetingSubmit(@ModelAttribute EventForm eventForm, HttpServletRequest request) {
-        eventForm.setIpAddress(request.getRemoteAddr());
-        TelegramBot.events.add(eventForm);
+        String ipAddr = request.getRemoteAddr();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        // if it is a new day, revert block.
+        if ( lockedDate.containsKey(ipAddr) &&  !dtf.format(LocalDate.now()).equals(lockedDate.get(ipAddr))) {
+            lockedDate.remove(ipAddr);
+            ipAddressesAndCalls.put(ipAddr,1);
+        }
+        if ( ipAddressesAndCalls.containsKey(ipAddr)) {
+            ipAddressesAndCalls.put(ipAddr, ipAddressesAndCalls.get(ipAddr) +1);
+        } else {
+            ipAddressesAndCalls.put(ipAddr,1);
+        }
+        if ( ipAddressesAndCalls.get(ipAddr) <10) {
+            eventForm.setIpAddress(ipAddr);
+            TelegramBot.events.add(eventForm);
+        } else {
+            if ( !lockedDate.containsKey(ipAddr)) {
+                String newEntryPoint = dtf.format(LocalDate.now());
+                lockedDate.put(ipAddr, newEntryPoint);
+            }
+            return "sadPony";
+        }
+
+
         return "calendar";
     }
 }
